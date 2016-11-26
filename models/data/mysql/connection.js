cf782cb195793777be0bc85ddc658156b70fb4e1
alex alonso <alex.xmde@gmail.com>
2016-11-26 14:10:07 -0200

    Openshift all over again

--- models/data/mysql/connection.js
+++ models/data/mysql/connection.js
@@ -3,39 +3,41 @@ var fs = require('fs');
 console.log("Selected Environment: " + process.env.dbenvironment);
 
 try{
-switch (process.env.dbenvironment) {
+  switch (process.env.dbenvironment) {
 
-  case "development":
-  var credentials = JSON.parse(
-    fs.readFileSync(__dirname + '/credentials.json', 'utf-8')
-  ).development;
-  break;
+    case "development":
+      var credentials = JSON.parse(
+        fs.readFileSync(__dirname + '/credentials.json', 'utf-8')
+      ).development;
+      break;
 
-  case "test":
-  var credentials = JSON.parse(
-    fs.readFileSync(__dirname + '/credentials.json', 'utf-8')
-  ).test;
-  break;
+    case "test":
+      var credentials = JSON.parse(
+        fs.readFileSync(__dirname + '/credentials.json', 'utf-8')
+      ).test;
+      break;
 
-  case "production":
-    var credentials = JSON.parse(fs.readFileSync(__dirname + "/credentials.json", "utf-8"))
-    .production;
+    case "production":
+      var credentials = JSON.parse(fs.readFileSync(__dirname + "/credentials.json", "utf-8"))
+        .production;
 
-}
+      var connection = mysql.createConnection({
+        host: process.env.OPENSHIFT_MYSQL_DB || credentials.url,
+        port: process.env.OPENSHIFT_MYSQL_DB_PORT || credentials.port,
+        user: credentials.user,
+        password : credentials.password,
+        database : credentials.database
+      });
+
+
+      module.exports = connection;
+  }
 }
 catch (e) {
- 
+  console.log(e);
+
 }
 
 
 
-var connection = mysql.createConnection({
-  host: process.env.OPENSHIFT_MYSQL_DB || credentials.url,
-  port: process.env.OPENSHIFT_MYSQL_DB_PORT || credentials.port,
-  user: credentials.user,
-  password : credentials.password,
-  database : credentials.database
-});
 
-
-module.exports = connection;
