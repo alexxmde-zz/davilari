f44941efda0fc3c118657b2f96e91c25a719a9d7
Alex Alonso <aalonso@localhost.localdomain>
2016-12-13 13:48:02 -0200

    Imagens

---
+++ views/index.js
@@ -0,0 +1,45 @@
+process.env.dbenvironment = "development";
+
+//Imports
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  cookieParser = require('cookie-parser'),
+  session = require('express-session'),
+  adminRouter = require('./routes/admin'),
+  publicRouter = require('./routes/public'),
+  sessionCheck = require('./middlewares/session'),
+
+
+
+//IP Address
+app = express(),
+ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+port = process.env.OPENSHIFT_NODEJS_PORT || '8000'; 
+//BodyParser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//Session
+app.use(cookieParser());
+app.use(session({secret: 'donniebrasco'}));
+
+//View Engine
+app.set('view engine', 'ejs');
+
+//Routes
+app.use('/resources', express.static('resources'));
+app.use('/admin', sessionCheck,  adminRouter);
+app.use(publicRouter);
+
+
+//Server Start
+app.listen(port, function(err) {
+  if (err) {
+    console.error("Erro ao iniciar servidor: " + err);
+    return;
+  }
+
+  console.log("Server started at: " + ip + " , port: " + port);
+
+});
+
