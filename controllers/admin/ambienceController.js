cc9f097a5fb3b5854887666875100206817cf8b3
OpenShift System User <583830c72d52711d1d000119@ex-std-node585.prod.rhcloud.com>
2017-03-04 14:40:11 -0500

    a

--- controllers/admin/ambienceController.js
+++ controllers/admin/ambienceController.js
@@ -4,15 +4,15 @@ class AmbienceController {
 
   renderIndex(req, res) {
     let findAmbs =   ambiencesModel.find({}).exec();
-    findAmbs.then((ambs) => { 
-        res.render('admin/pages/ambience', {'ambiences' : ambs});
-        });
+    findAmbs.then((ambs) => {
+      res.render('admin/pages/ambience', {'ambiences' : ambs});
+    });
 
     findAmbs.catch(err=> {
-        res.status(500).send(err);
-        })
+      res.status(500).send(err);
+    })
   }
-  
+
   renderAmbience(req, res) {
     let findAmb = ambiencesModel.findById(req.params.id).exec();
     findAmb.then(amb => {
@@ -31,15 +31,16 @@ class AmbienceController {
 
     ambience.mainImage = req.files['mainImage'][0].filename;
     ambience.images = [];
-    req.files['images']
+    if (req.files['images']) {
+      req.files['images']
       .forEach(function ReturnImage (img) {
-          ambience.images.push(img.filename);
-          });
-
+        ambience.images.push(img.filename);
+      });
+    }
     let save = ambience.save();
     save.then(() => res.send())
     save.catch(err => res.status(500).send(err));
-   
+
   }
 
   updateAmbience (req, res) {
@@ -49,8 +50,8 @@ class AmbienceController {
     if (req.files['images']) {
 
       ambience.images = req.files['images'].map(function (obj) {
-          return obj.filename;
-          });
+        return obj.filename;
+      });
     }
 
     if (req.files['mainImage']) {
